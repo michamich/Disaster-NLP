e0e9bd2f3b59ccde62d61d9674a460dc9968eadb
Michael Surjawidjaja <michael.surjawidjaja@gmail.com>
2020-03-14 15:46:38 +0800

    Improve formatting algorithm
    Implement method to remove punctuations and tags.
    Add formatting.json that contains string formatting.

--- nlp_disaster/methods/formatting.py
+++ nlp_disaster/methods/formatting.py
@@ -8,3 +8,13 @@ def clean_stopwords(sentence):
     filtered_sentence = [w for w in word_tokens if not w in stop_words]
 
     return filtered_sentence
+
+def clean_puncts(sentence, formatting_dict):
+    for k, v in formatting_dict.items():
+        sentence = sentence.replace(k, v)
+    sentence_split = sentence.split(" ")
+    out_list = []
+    for word in sentence_split:
+        if "@" not in word:
+            out_list.append(word)
+    return " ".join(out_list)
